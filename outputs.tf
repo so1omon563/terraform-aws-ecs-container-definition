1c8570275996cb76ae74411f93553778a1941056
Jedidiah Foster <jedidiah.foster@gmail.com>
2022-11-03 06:33:48 -0700

    Clarification on outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "json" {
-  description = "The JSON representation of the container definition. This can be used to pass to the `container_definitions` argument of the `aws_ecs_task_definition` resource."
+  description = "The JSON representation of the container definition. This can be used to pass to the `container_definitions` argument of the `aws_ecs_task_definition` resource. May require using `jsondecode` to facilitate this."
   value       = jsonencode(local.definition)
 }
 
